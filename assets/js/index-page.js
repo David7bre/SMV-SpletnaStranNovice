36002a8f8e36244733c17651780e6ed267c8dd3c
David Brezovnik <brezovnik.david386@gmail.com>
2025-11-12 20:16:00 +0100

    new news popup progress

--- assets/js/index-page.js
+++ assets/js/index-page.js
@@ -1,6 +1,22 @@
+let popup, backdrop;
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.add-news').addEventListener('click', () => {
-        console.log('test')
-        document.querySelector('.add-news-popup').classList.toggle('hidden')
+    popup = document.querySelector('.add-news-popup')
+    backdrop = document.querySelector('.add-news-popup-backdrop')
+
+    document.querySelector('.add-news').addEventListener('click', togglePopup)
+
+    document.querySelector('.add-news-popup-backdrop').addEventListener('click', () => {
+        if (!popup.classList.contains('hidden')) togglePopup()
+    })
+
+    document.addEventListener('keydown', e => {
+        if (e.key === "Escape" && !popup.classList.contains('hidden')) togglePopup()
     })
 })
+
+function togglePopup() {
+    [popup, backdrop].forEach(el => {
+        el.classList.toggle('hidden')
+    })
+}
